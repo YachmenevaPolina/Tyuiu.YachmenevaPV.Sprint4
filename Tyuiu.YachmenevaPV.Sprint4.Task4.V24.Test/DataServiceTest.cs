8fc239ed68d2a463c04543f303ae5953bceb7d9b
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-11-08 23:41:56 +0500

    Task4

---
+++ Tyuiu.YachmenevaPV.Sprint4.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.YachmenevaPV.Sprint4.Task4.V24.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint4.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas = { { 7, 9, 7, 6, 7 }, { 9, 9, 8, 6, 7 }, { 8, 6, 5, 6, 7 }, { 9, 9, 7, 8, 7 }, { 5, 9, 9, 8, 9 } };
+            int [,] res = ds.Calculate(mas);
+            int[,] wait = { { 7, 9, 7, 1, 7 }, { 9, 9, 1, 1, 7 }, { 1, 1, 5, 1, 7 }, { 9, 9, 7, 1, 7 }, { 5, 9, 9, 1, 9 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}

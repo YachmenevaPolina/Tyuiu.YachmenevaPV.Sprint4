2b8aba0e10d3701ce85d46bd0f4b0141bfec8e9a
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-11-08 22:31:14 +0500

    Task2

---
+++ Tyuiu.YachmenevaPV.Sprint4.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.YachmenevaPV.Sprint4.Task2.V19.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint4.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 5, 6, 7, 7, 8, 9, 6, 5, 4, 8, 7, 6, 5 };
+            int res = ds.Calculate(numsArray);
+            int wait = 45;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint4.Task2.V19/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.YachmenevaPV.Sprint4.Task2.V19.Lib;
+{
+    DataService ds = new DataService();
+    Random rnd = new Random();
+    Console.Title = "Спринт #4 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #4                                                               *");
+    Console.WriteLine("* Тема: Одномерные массивы.                                               *");
+    Console.WriteLine("* Задание #2                                                              *");
+    Console.WriteLine("* Вариант #19                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Посчитать сумму нечетных элементов массива.                             *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Введите количество элементов массива: ");
+    int len = Convert.ToInt32(Console.ReadLine());
+
+    int[] numsArray = new int[len];
+
+    for (int i = 0; i <= len - 1; i++) { numsArray[i] = rnd.Next(4, 9); }
+
+    Console.WriteLine("Массив: ");
+    for (int i = 0;i <= len - 1; i++) {Console.Write(numsArray[i] + "\t"); }
+
+    Console.WriteLine();
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    int res = ds.Calculate(numsArray);
+
+    Console.WriteLine("Сумма нечетных элементов = " + res);
+
+    Console.ReadKey();
+}

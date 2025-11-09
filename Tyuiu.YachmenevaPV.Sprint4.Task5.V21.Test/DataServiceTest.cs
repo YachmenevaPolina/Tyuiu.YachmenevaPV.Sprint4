4e495f2c952aa4bfd9c07185e927826372c966f2
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-11-09 17:37:21 +0500

    Task5

---
+++ Tyuiu.YachmenevaPV.Sprint4.Task5.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.YachmenevaPV.Sprint4.Task5.V21.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint4.Task5.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] mas = { { -8, 0, 2, -4, 1 }, { -6, -2, 0, 1, 2}, { -7, -3, -4, 0, 1}, { 0, 2, -3, -1, 0}, { -6, -4, -3, -5, -1}  };
+            int[,] res = ds.Calculate(mas);
+            int[,] wait = { { -8, 0, 1, -4, 1 }, { -6, -2, 0, 1, 1 }, { -7, -3, -4, 0, 1 }, { 0, 1, -3, -1, 0 }, { -6, -4, -3, -5, -1 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint4.Task5.V21/Program.cs
@@ -0,0 +1,64 @@
+﻿using Tyuiu.YachmenevaPV.Sprint4.Task5.V21.Lib;
+{
+    DataService ds = new DataService();
+    Random rnd = new Random();
+    Console.Title = "Спринт #4 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #4                                                               *");
+    Console.WriteLine("* Тема: Двумерные массивы.                                                *");
+    Console.WriteLine("* Задание #5                                                              *");
+    Console.WriteLine("* Вариант #21                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Заменить положительные элементы большие  массива на 1.                  *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Введите количество строк в массиве: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите количество столбцов в массиве: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] matrix = new int[rows, columns];
+
+    Console.WriteLine("***************************************************************************");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(-8, 2);
+        }
+    }
+    Console.WriteLine("\nМассив: ");
+    for (int i = 0; i < rows;  ++i)
+    {
+        for (int j = 0; j < columns;  ++j)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine("\r");
+    }
+    Console.WriteLine();
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    int [,] res = ds.Calculate(matrix);
+
+    int rowss = res.GetUpperBound(0) + 1;
+    int columnss = res.Length / rowss;
+    for (int i = 0; i < rowss; i++)
+    {
+        for (int j = 0; j < columnss; j++)
+        {
+            Console.Write($"{res[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.ReadKey();
+}
